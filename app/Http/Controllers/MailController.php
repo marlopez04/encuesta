2e77b5361a2125824e1be93857d291b24e69f0c1
marlopez04 <marlopez04@hotmail.com>
2020-11-19 17:41:23 -0300

    prueba de mail 2

--- app/Http/Controllers/MailController.php
+++ app/Http/Controllers/MailController.php
@@ -8,6 +8,7 @@ use Mail;
 use Session;
 use Redirect;
 use App\Encuestado;
+use App\Email;
 
 use App\Http\Requests;
 
@@ -20,13 +21,52 @@ class MailController extends Controller
      */
     public function index()
     {
-        $encuestado = Encuestado::find(300);
+        //$encuestado = Encuestado::find(309);
         //dd($encuestado);
-        $array[] = 
-        Mail::send('encuesta.mail.encuestado', ['encuestado' => $encuestado], function($msj){
+
+        /*
+        $mails = Email::all();
+
+        foreach ($mails as $key => $mailencuestado) {
+            # code...
+        }
+        //dd($mails);
+
+        */
+
+        $encuestado = new Encuestado();
+        $encuestado->descripcion = " ";
+        $encuestado->area_id = 0;
+        $encuestado->rangoedad_id = 0;
+        $encuestado->genero_id = 0;
+        $encuestado->encuesta_id = 6;
+        $encuestado->antiguedad_id = 0;
+        $encuestado->sector_id = 0;
+        $encuestado->puesto_id = 0;
+        $encuestado->estudio_id = 0;
+        $encuestado->sede_id = 0;
+        $encuestado->contrato_id = 0;
+        $encuestado->save();
+
+        $direccion = "http://35.238.99.14/encuesta/encuestado/" . $encuestado->id . "/edit";
+
+        //dd($direccion);
+
+/*
+
+        return view('encuesta.mail.encuestado')
+            ->with('direccion',$direccion);
+
+*/
+
+        Mail::send('encuesta.mail.encuestado', ['direccion' => $direccion], function($msj){
             $msj->subject('Encuesta de Clima 2020');
-            $msj->to('marlopez04@hotmail.com');
+            $msj->to('mlmorales@cofaral.com.ar');
+            //$msj->to('abarreto@cofaral.com.ar');
         });
+
+
+
 
 /*      //ejemplo de codigo
 
---
+++ resources/views/encuesta/mail/encuestado.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+<body>
+    Lo invitamos a llenar el siguiente cuestionario 
+    <p>
+    {{$direccion}}
+    <p>
+    <p>
+    <a href="{{$direccion}}">Llenar encuesta 1</a>
+    <p>
+    <a href="<?php echo $direccion; ?>">Llenar encuesta 2</a>
+    
+
+    
+</body>
+</html>
